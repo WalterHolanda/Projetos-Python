4521b63f5868c829b633568a0f43dc362ba122b5
Walter Holanda <123330079+WalterHolanda@users.noreply.github.com>
2023-02-22 12:56:02 -0300

    Novos exercícios

---
+++ CursoPython/Exercícios/Ex2.py
@@ -0,0 +1,10 @@
+#ex2
+nome1 = input('Qual o seu nome? ')
+print('Olá',nome1 ,'!seja bem vindo!')
+#ou
+nome2 = input('Qual o seu nome? ')
+print('Olá {}! seja bem vindo!'.format(nome2))
+#ou
+nome3 = input('Qual o seu nome? ')
+print(f'Olá {nome3}! seja bem vindo!')
+print(nome1, nome2, nome3)
--- CursoPython/Exercícios/Ex3.py
+++ CursoPython/Exercícios/Ex3.py
@@ -1,7 +1,3 @@
-#ex1
-nome = input('Qual o seu nome? ')
-print('Olá',nome ,'! seja bem vindo!')
-
 #ex3;
 num1 = int (input('digite um número '))
 num2 = int (input('digite outro número '))
