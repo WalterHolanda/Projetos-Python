b7c5f3eb5bd017998985de3867efe15b5775cb0e
Walter Holanda <123330079+WalterHolanda@users.noreply.github.com>
2023-02-22 12:25:56 -0300

    Create Ex1.py

---
+++ CursoPython/Exercícios/Ex1.py
@@ -0,0 +1,9 @@
+#ex1
+nome = input('Qual o seu nome? ')
+print('Olá',nome ,'! seja bem vindo!')
+
+#ex3;
+num1 = int (input('digite um número '))
+num2 = int (input('digite outro número '))
+s = num1 + num2
+print('A soma entre {} e {} é {}' .format(num1,num2,s))
